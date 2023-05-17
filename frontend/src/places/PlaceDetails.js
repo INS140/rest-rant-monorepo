f71ee876df1967d5afae14e027ae48ac1de11e8e
INS140 <IsaacNSaettel@gmail.com>
2023-05-17 04:23:01 -0700

    role-based auth

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router"
 import CommentCard from './CommentCard'
 import NewCommentForm from "./NewCommentForm";
+import { CurrentUser } from "../contexts/CurrentUser";
 
 function PlaceDetails() {
+
+	const { currentUser } = useContext(CurrentUser)
 
 	const { placeId } = useParams()
 
@@ -103,6 +106,18 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions
+
+	if (currentUser?.role === 'admin') {
+		placeActions = <>
+			<a className="btn btn-warning" onClick={editPlace}>
+				Edit
+			</a>{` `}
+			<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+				Delete
+			</button>
+		</>
+	}
 
 	return (
 		<main>
@@ -130,12 +145,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
